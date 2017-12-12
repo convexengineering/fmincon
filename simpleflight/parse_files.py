089ad762971cdda66e8f9c45baf6604c0bd43b7d
pgkirsch <kirschen@mit.edu>
2017-12-11 20:21:48 -0800

    Updated results for simpleflight

---
+++ simpleflight/parse_files.py
@@ -0,0 +1,36 @@
+import os
+
+algorithms = ['interior-point', 'SQP']
+guesstypes = ['order-of-magnitude-floor', 'order-of-magnitude-round', 'order-of-magnitude-mix',
+              'almost-exact-solution', 'ones']
+gradobjs = ['on', 'off']
+gradconstrs = ['on', 'off']
+
+excludedcases = ['interior-point/order-of-magnitude-floor/on/on']
+
+with open('results.txt', 'w') as outfile:
+    outfile.write('#Results Table\n') 
+
+for algorithm in algorithms:
+    for guesstype in guesstypes:
+        for gradobj in gradobjs:
+            for gradconstr in gradconstrs:
+                if ((gradobj is 'on' and gradconstr is 'on') or
+                    (gradobj is 'off' and gradconstr is 'off')):
+                    result = {}
+                    directory = (algorithm + '/' + guesstype + '/' + gradobj + '/' +
+                                 gradconstr + '/')
+                    with open('results.txt', 'a') as outfile:
+                        outfile.write('{0:<48}'.format(directory) + ' ')
+                    if os.path.exists(directory):
+                        for metric in ['cost', 'iterations', 'elapsed']:
+                            if os.path.exists(directory + metric + '.txt'):
+                                with open(directory + metric + '.txt', 'r') as f:
+                                    result[metric] = f.readline()
+
+                                with open('results.txt', 'a') as outfile:
+                                    outfile.write('{0:>14}'.format(result[metric]) + ' ')
+                    with open('results.txt', 'a') as outfile:
+                        outfile.write('\n')
+                
+
