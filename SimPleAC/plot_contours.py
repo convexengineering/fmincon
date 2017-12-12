ea0f8e3bc45acc785c5f6e9355e30d15b0cdf20d
pgkirsch <kirschen@mit.edu>
2017-12-11 20:01:02 -0800

    Update contour plot markers

--- SimPleAC/plot_contours.py
+++ SimPleAC/plot_contours.py
@@ -24,8 +24,8 @@ levels = [3600, 3800, 4000, 4200, 4400, 4600, 4800,
 #levels = [4, 5, 6, 7, 8, 9, 10, 11, 12]
 CS = plt.contour(X, Y, Z, levels=levels, colors='k')
 plt.clabel(CS, inline=1, fontsize=10, fmt='%1.0f')
-ax.plot(0.1047, 0.4613, 'o', color='r', markersize=10)
-ax.plot(0.1050, 0.4630, 's', color='g', markersize=10)
+ax.plot(0.1047, 0.4613, '>', color='r', markersize=10)
+ax.plot(0.1050, 0.4630, '<', color='g', markersize=10)
 ax.plot(0.1602, 0.4813, '^', color='b', markersize=10)
 ax.set_xlabel(r'$V_{f_{wing}} [m^3]$', fontsize=16)
 ax.set_ylabel(r'$V_{f_{fuse}} [m^3]$', fontsize=16)
